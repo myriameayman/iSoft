1b29bc0f6c962a6c7c2902386b951c568b6c8707
myriameayman <myriameayman@gmail.com>
2015-02-22 21:00:30 +0200

    myriame merging

--- blog/app/controllers/application_controller.rb
+++ blog/app/controllers/application_controller.rb
@@ -1,5 +1,20 @@
-class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+
+class ApplicationController < ActionController::Base 
+# Prevent CSRF attacks by raising an exception.
+ # For APIs, you may want to use :null_session instead. 
+ protected 
+ def login_required 
+ return true 
+ if User.find_by_id(session[:user_id]) 
+ 	access_denied 
+    return false 
+end 
+end 
+ def access_denied 
+ 	flash[:error] = 'Oops. You need to login before you can view that page.' 
+ 	redirect_to :log_in 
+ end
   protect_from_forgery with: :exception
-end
+  end
+
+
--- blog/db/migrate/20150219214727_create_users.rb
+++ blog/db/migrate/20150219214727_create_users.rb
@@ -1,4 +1,6 @@
+
 class CreateUsers < ActiveRecord::Migration
+
   def change
     create_table :users do |t|
       t.string :name

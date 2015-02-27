ecae2efa9a4d4fd3c8b9591e29e4d0ec963d0c19
myriameayman <myriameayman@gmail.com>
2015-02-27 23:43:04 +0200

    images

--- blog/config/environments/production.rb
+++ blog/config/environments/production.rb
@@ -15,7 +15,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
   config.serve_static_assets = true 
   config.assets.compile = true 
-  config.assets.digest = true
+  
   config.serve_static_assets = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
@@ -27,18 +27,16 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.serve_static_files = ENV['RAILS_SERVE_STATIC_FILES'].present?
-
+  config.assets.digest = true
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
-  config.assets.digest = true
-
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Specifies the header that your server uses for sending files.

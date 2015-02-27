5996b6c7cc7e65ae2e136566bdc3084964093631
myriameayman <myriameayman@gmail.com>
2015-02-27 17:55:49 +0200

    lesamsh bayna

--- blog/config/environments/production.rb
+++ blog/config/environments/production.rb
@@ -13,6 +13,9 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.serve_static_assets = true 
+  config.assets.compile = true 
+  config.assets.digest = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.

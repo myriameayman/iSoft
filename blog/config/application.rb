212cd9196d4ec1af546581767d02bcdeb9489a7d
myriameayman <myriameayman@gmail.com>
2015-02-27 14:29:33 +0200

    trying to deploy

--- blog/config/application.rb
+++ blog/config/application.rb
@@ -23,5 +23,6 @@ module Blog
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.assets.initialize_on_precompile = false
     config.active_record.raise_in_transactional_callbacks = true
+    config.serve_static_assets = true
   end
 end

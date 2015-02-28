83c2bf089f67d48fe4f011c86f16e0e30c4d62d0
ebrahim-elgaml <ebrahim.el.gaml@gmail.com>
2015-02-28 02:09:03 +0200

    bunfle

--- blog/db/schema.rb
+++ blog/db/schema.rb
@@ -65,8 +65,12 @@ ActiveRecord::Schema.define(version: 20150222104353) do
     t.string   "email"
     t.string   "password_hash"
     t.string   "password_salt"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
